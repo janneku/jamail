f563a2d0efee66e5976bc0f3492f6545c521b06d
Janne Kulmala <janne.t.kulmala@tut.fi>
2011-12-13 22:56:35 +0200

    utils: Optimize to_unicode() [PERFECTIVE]

--- utils.cc
+++ utils.cc
@@ -1,15 +1,14 @@
 #include "utils.h"
 
-ustring to_unicode(const std::string &s)
+ustring to_unicode(const std::string &in)
 {
-	ustring out;
-	for (size_t i = 0; i < s.size(); ++i) {
-		unsigned char c = s[i];
-		if (c < 128) {
-			out += c;
-		} else {
-			out += '?';
+	ustring out(in.size(), 0);
+	for (size_t i = 0; i < in.size(); ++i) {
+		unsigned char c = in[i];
+		if (c >= 128) {
+			c = '?';
 		}
+		out[i] = c;
 	}
 	return out;
 }
