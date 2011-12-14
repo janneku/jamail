f57d4faa88c61a40e40ecff1e9ed66ddbadd40d0
Janne Kulmala <janne.t.kulmala@tut.fi>
2011-12-14 15:38:04 +0200

    Update STL enconding library [PERFECTIVE]

--- encoding.cc
+++ encoding.cc
@@ -1,89 +1,145 @@
 /*
- * A small JSON parser and serializer.
+ * STL streams that perform unicode encoding and decoding (with iconv)
  *
  * Copyright 2011 Janne Kulmala <janne.t.kulmala@iki.fi>
  *
  * See LICENSE file for license.
  *
- * Methods for converting encoding and decoding unicode strings (using iconv)
  */
 #include "encoding.h"
 #include <assert.h>
 #include <iconv.h>
+#include <sstream>
+#include <string.h>
 #include <errno.h>
 
 std::string encode(const ustring &in, const char *enc)
 {
-	char *in_ptr = (char *) &in[0];
-	size_t in_left = in.size() * sizeof(in[0]);
-
-	/*
-	 * Strings are stored as sequences of UTF-32 characters stored in
-	 * native byte order. Feed the string to iconv chunk at a time while
-	 * allocating more room for the output. iconv will advance the input
-	 * pointer for us.
-	 */
-	iconv_t conv = iconv_open(enc, "UTF-32");
-	if (conv == iconv_t(-1)) {
-		throw conv_error("Unable to initialize iconv");
-	}
+	std::basic_istringstream<uint32_t> source(in);
+	enc_streambuf sb(source, enc);
+	std::istream is(&sb);
 
 	std::string out;
-	while (in_left > 0) {
+	while (1) {
 		size_t pos = out.size();
 		out.resize(pos + 256, 0);
-		char *out_ptr = &out[pos];
-		size_t out_left = 256;
-		size_t ret =
-			iconv(conv, &in_ptr, &in_left, &out_ptr, &out_left);
-		if (ret == size_t(-1)) {
-			if (errno == EILSEQ) {
-				throw conv_error("Invalid char sequence");
-			} else if (errno != E2BIG) {
-				throw conv_error("Conversion failed");
-			}
+		if (!is.read(&out[pos], 256)) {
+			out.resize(pos + is.gcount());
+			break;
 		}
-		out.resize(pos + 256 - out_left);
 	}
-	iconv_close(conv);
 	return out;
 }
 
 ustring decode(const std::string &in, const char *enc)
 {
-	char *in_ptr = (char *) &in[0];
-	size_t in_left = in.size();
+	std::istringstream source(in);
+	dec_streambuf sb(source, enc);
+	std::basic_istream<uint32_t> is(&sb);
+
+	ustring out;
+	while (1) {
+		size_t pos = out.size();
+		out.resize(pos + 256, 0);
+		if (!is.read(&out[pos], 256)) {
+			out.resize(pos + is.gcount());
+			break;
+		}
+	}
+	return out;
+}
+
+enc_streambuf::enc_streambuf(std::basic_istream<uint32_t> &source,
+			     const std::string &enc) :
+	m_source(source), m_enc(enc), m_input(NULL), m_left(0)
+{
+}
+
+enc_streambuf::int_type enc_streambuf::underflow()
+{
+	fillbuf();
+	if (m_left == 0) {
+		return traits_type::eof();
+	}
+
+	iconv_t conv = iconv_open(m_enc.c_str(), "UTF-32");
+	if (conv == iconv_t(-1)) {
+		throw conv_error("Unable to initialize iconv");
+	}
+
+	char *out_ptr = m_buf;
+	size_t out_left = sizeof m_buf;
+
+	size_t ret = iconv(conv, &m_input, &m_left, &out_ptr, &out_left);
+	if (ret == size_t(-1)) {
+		if (errno == EILSEQ) {
+			throw conv_error("Invalid char sequence");
+		} else if (errno != E2BIG) {
+			throw conv_error("Conversion failed");
+		}
+	}
+	iconv_close(conv);
+	setg(m_buf, m_buf, out_ptr);
+	return m_buf[0];
+}
+
+void enc_streambuf::fillbuf()
+{
+	memmove(m_readbuf, m_input, m_left);
+	m_input = (char *) m_readbuf;
+
+	/* Note, m_left is in bytes */
+	m_source.read(&m_readbuf[m_left / sizeof(uint32_t)],
+		      (sizeof m_readbuf - m_left) / sizeof(uint32_t));
+	m_left += m_source.gcount() * sizeof(uint32_t);
+}
+
+dec_streambuf::dec_streambuf(std::istream &source, const std::string &enc) :
+	m_source(source), m_enc(enc), m_input(NULL), m_left(0)
+{
+}
+
+dec_streambuf::int_type dec_streambuf::underflow()
+{
+	fillbuf();
+	if (m_left == 0) {
+		return traits_type::eof();
+	}
 
 	/*
 	 * iconv likes to write an UTF-32 BOM to the beginning of the output
 	 * if the used byte order is not specified.
 	 */
 #if (BYTE_ORDER == LITTLE_ENDIAN)
-	iconv_t conv = iconv_open("UTF-32LE", enc);
+	iconv_t conv = iconv_open("UTF-32LE", m_enc.c_str());
 #else
-	iconv_t conv = iconv_open("UTF-32BE", enc);
+	iconv_t conv = iconv_open("UTF-32BE", m_enc.c_str());
 #endif
 	if (conv == iconv_t(-1)) {
 		throw conv_error("Unable to initialize iconv");
 	}
 
-	ustring out;
-	while (in_left > 0) {
-		size_t pos = out.size();
-		out.resize(pos + 256, 0);
-		char *out_ptr = (char *) &out[pos];
-		size_t out_left = 256 * sizeof(uint32_t);
-		size_t ret =
-			iconv(conv, &in_ptr, &in_left, &out_ptr, &out_left);
-		if (ret == size_t(-1)) {
-			if (errno == EILSEQ) {
-				throw conv_error("Invalid char sequence");
-			} else if (errno != E2BIG) {
-				throw conv_error("Conversion failed");
-			}
+	char *out_ptr = (char *) m_buf;
+	size_t out_left = sizeof m_buf;
+
+	size_t ret = iconv(conv, &m_input, &m_left, &out_ptr, &out_left);
+	if (ret == size_t(-1)) {
+		if (errno == EILSEQ) {
+			throw conv_error("Invalid char sequence");
+		} else if (errno != E2BIG) {
+			throw conv_error("Conversion failed");
 		}
-		out.resize(pos + 256 - out_left / sizeof(uint32_t));
 	}
 	iconv_close(conv);
-	return out;
+	setg(m_buf, m_buf, (uint32_t *) out_ptr);
+	return m_buf[0];
+}
+
+void dec_streambuf::fillbuf()
+{
+	memmove(m_readbuf, m_input, m_left);
+	m_input = m_readbuf;
+
+	m_source.read(&m_readbuf[m_left], sizeof m_readbuf - m_left);
+	m_left += m_source.gcount();
 }
--- encoding.h
+++ encoding.h
@@ -3,7 +3,10 @@
 
 #include <stdexcept>
 #include <stdint.h>
+#include <streambuf>
+#include <istream>
 
+/* An unicode string (UTF-32) */
 typedef std::basic_string<uint32_t> ustring;
 
 class conv_error: public std::runtime_error {
@@ -15,5 +18,38 @@ public:
 
 std::string encode(const ustring &in, const char *enc);
 ustring decode(const std::string &in, const char *enc);
+
+class dec_streambuf: public std::basic_streambuf<uint32_t> {
+public:
+	dec_streambuf(std::istream &source, const std::string &enc);
+
+private:
+	std::istream &m_source;
+	std::string m_enc;
+	char m_readbuf[4096];
+	char *m_input;
+	size_t m_left;
+	uint32_t m_buf[256];
+
+	int_type underflow();
+	void fillbuf();
+};
+
+class enc_streambuf: public std::streambuf {
+public:
+	enc_streambuf(std::basic_istream<uint32_t> &source,
+		      const std::string &enc);
+
+private:
+	std::basic_istream<uint32_t> &m_source;
+	std::string m_enc;
+	uint32_t m_readbuf[1024];
+	char *m_input;
+	size_t m_left;
+	char m_buf[1024];
+
+	int_type underflow();
+	void fillbuf();
+};
 
 #endif
